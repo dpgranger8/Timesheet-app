eaa52abf05139528e7dca014e543a78a96a48d8b
David Granger <dpgranger8@gmail.com>
2025-09-30 15:18:22 -0600

    displayed list of departments

--- src/app/components/departments/departments.ts
+++ src/app/components/departments/departments.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { DepartmentsService } from '../../services/departments';
 import { Department } from '../../interfaces/department';
+import { MatCardModule } from '@angular/material/card'
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-departments',
   standalone: true,
+  imports: [
+    MatCardModule,
+    MatListModule,
+  ],
   templateUrl: './departments.html',
   styleUrls: ['./departments.scss']
 })
